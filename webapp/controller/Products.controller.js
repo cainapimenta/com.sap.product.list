5216ce800cd2a4bbcf0f8c24016bdde786a97bf5
Caina Pimenta <08137abb-f9ec-419e-9d3b-21e517d31087>
2025-09-17 21:56:45 +0000

    mudanças

--- webapp/controller/Products.controller.js
+++ webapp/controller/Products.controller.js
@@ -2,38 +2,72 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/m/MessageBox",
-    "sap/ui/model/json/JSONModel"
-], (Controller, MessageToast, MessageBox, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/odata/v2/ODataModel"
+], (Controller, MessageToast, MessageBox, JSONModel, ODataModel) => {
     "use strict";
 
     return Controller.extend("com.alfa.products.list.controller.Products", {
         onInit() {
-            // Simulando backend
-            // GET /frutas
-            const frutas = [
-                {
-                    id: 1,
-                    name: "Banana",
-                    quantity: 20
-                },
-                {
-                    id: 2,
-                    name: "Maça",
-                    quantity: 18
-                },
-                {
-                    id: 2,
-                    name: "Kiwi",
-                    quantity: 2
-                }
-            ];
 
-            const model = new JSONModel(frutas);
-            this.getView().setModel(model, 'frutas');
+            const productList = this.byId('productListId');
+            productList.setBusy(true);
+
+            const oDataModel = new ODataModel('/V2/Northwind/Northwind.svc/');
+            oDataModel.read('/Products', {
+                success: (oProducts) => {
+                    console.log(oProducts);
+
+                    const aProducts = oProducts.results;
+                    const model = new JSONModel(aProducts);
+                    this.getView().setModel(model, "products");
+
+                    productList.setBusy(false);
+                },
+                error: (ex) => {
+                    productList.setBusy(false);
+
+                    MessageBox.error(ex.message, {
+                        title: "Erro na requisição"
+                    });
+                }
+            });
+
+
+            // // Simulando backend
+            // // GET /frutas
+            // const frutas = [
+            //     {
+            //         id: 1,
+            //         name: "Banana",
+            //         quantity: 20
+            //     },
+            //     {
+            //         id: 2,
+            //         name: "Maça",
+            //         quantity: 18
+            //     },
+            //     {
+            //         id: 2,
+            //         name: "Kiwi",
+            //         quantity: 2
+            //     }
+            // ];
+
+            // const model = new JSONModel(frutas);
+            // this.getView().setModel(model, 'frutas');
         },
 
-        onPressListItem() {
-            MessageToast.show('teste');
+        onPressListItem(oEvent) {
+            const oSource = oEvent.getSource();
+            const titleItem = oSource.getTitle();
+
+            const oComponent = this.getOwnerComponent();
+            const i18nModel = oComponent.getModel('i18n');
+            const i18nBundle = i18nModel.getResourceBundle();
+            const msg = i18nBundle.getText('messageOnClick', [titleItem])
+
+            MessageToast.show(msg);
         },
         onPressErrorListItem() {
             MessageBox.error('Erro, você precisa revisar.',
