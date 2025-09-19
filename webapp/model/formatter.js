3f3702300de456fdb136a53b1266cf1940e3d949
Caina Pimenta <08137abb-f9ec-419e-9d3b-21e517d31087>
2025-09-19 23:15:10 +0000

    Adicionando navigation

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,22 +1,39 @@
 sap.ui.define([
 
-],  () => {
+], () => {
     "use strict";
 
     return {
         toUpperCase(sText, sPrice) {
-            if(!sText) {
+            if (!sText) {
                 return sText;
             }
 
             return `${sText.toUpperCase()} - R$ ${sPrice}`;
         },
         multiply(num) {
-            if(!num) {
+            if (!num) {
                 return num;
             }
 
             return num;
+        },
+
+        priceStatus(sUnitPrice) {
+            if (!sUnitPrice) {
+                return sUnitPrice;
+            }
+
+            const nUnitPrice = Number(sUnitPrice);
+
+            if (sUnitPrice <= 20) {
+                return "Success";
+            }
+            else if (nUnitPrice > 20 && nUnitPrice < 50) {
+                return "Warning"
+            } else {
+                return "Error";
+            }
         }
     }
 })
